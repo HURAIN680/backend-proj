3cb11caad81fe5fc0c38600cf1ea8eefc4b65a7f
hurain shaikh <hurainshaikh680@gmail.com>
2025-10-24 20:49:39 +0530

    changes day 2

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { DB_NAME } from "./constants.js";
+
+dotenv.config({
+    path: "./config/.env"
+});
+
+
+connectDB();
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (err) => {
+            console.error("Failed to connect to MongoDB", err);
+            throw err;
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to MongoDB", error);
+        throw error;
+    }
+})();
+*/
