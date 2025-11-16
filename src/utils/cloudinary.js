df4d5523deecdfc291a17f13791500107ec47f8d
hurain shaikh <hurainshaikh680@gmail.com>
2025-11-16 15:13:51 +0530

    11th vd

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadImage = async (filePath) => {
+    try {
+        if (!filePath) {
+            throw new Error('No file path provided');
+        }
+        const result = await cloudinary.uploader.upload(filePath, {
+            resource_type: 'auto'
+        });
+        console.log("Upload successful:", result.secure_url);
+        return result;
+    } catch (error) {
+       fs.unlinkSync(filePath);
+       throw new Error('Error uploading image to Cloudinary');
+    }
+};
+
+export { uploadImage };
