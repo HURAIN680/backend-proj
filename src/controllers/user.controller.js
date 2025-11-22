177599087b5ff8ebc12f81a57d098ce20ade1104
hurain shaikh <hurainshaikh680@gmail.com>
2025-11-22 15:03:07 +0530

    ...

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import asyncHandler from '../utils/asyncHandler.js';
 const registerUser = asyncHandler(async (req, res) => {
         res.status(200).json({
         success: true,
-        message: 'User profile fetched successfully'
+        message: 'done'
     });
 });
 
