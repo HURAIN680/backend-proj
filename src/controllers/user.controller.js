30a6604056fc31b62744e6d31ffbb5ba6819f0ac
hurain shaikh <hurainshaikh680@gmail.com>
2025-11-29 14:36:09 +0530

    add change password functionality with validation and error handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -173,5 +173,43 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 });
 
 
+const chnagePassword = asyncHandler(async (req, res) => {
+    // get old password and new password from req body
+    // validate both are present
+    // find user from req.user._id
+    // check old password is correct
+    // set new password
+    // save user
+    // return res
+
+    const { oldPassword, newPassword } = req.body;
+    
+    if (!oldPassword || !newPassword) {
+        throw new ApiError(400, 'Old password and new password are required');
+    }
+
+   const user = await User.findById(req.user._id);
+
+   if (!user) {
+       throw new ApiError(404, 'User not found');
+   }
+
+   const isOldPasswordValid = await user.isPasswordMatch(oldPassword);
+   if (!isOldPasswordValid) {
+       throw new ApiError(401, 'Invalid old password');
+   }
+
+   user.password = newPassword;
+   await user.save();
+
+   return res.status(200).json(new ApiResponse(200, 'Password changed successfully'));
+ });
+
+ 
+
+
+    
+
+
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken };
