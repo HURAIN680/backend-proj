879abefe0fdcc6239cd93a560ab02f554f84b8d6
hurain shaikh <hurainshaikh680@gmail.com>
2025-12-06 20:59:43 +0530

    refactor: remove unused subscription controller methods and imports

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -6,10 +6,43 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
-const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
-})
+export const toggleSubscription = asyncHandler(async (req, res) => {
+    const { channelId } = req.params;
+    const userId = req.user._id;
+
+    // Validate Channel ID
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channel ID");
+    }
+
+    // Check if channel exists (optional but recommended)
+    const channel = await User.findById(channelId);
+    if (!channel) {
+        throw new ApiError(404, "Channel does not exist");
+    }
+
+    // Check if subscription already exists
+    const existingSubscription = await Subscription.findOne({
+        user: userId,
+        channel: channelId,
+    });
+
+    if (existingSubscription) {
+        await existingSubscription.deleteOne();
+        return ApiResponse.success(res, {
+            message: "Unsubscribed from channel",
+        });
+    }
+
+    await Subscription.create({
+        user: userId,
+        channel: channelId,
+    });
+
+    return ApiResponse.success(res, {
+        message: "Subscribed to channel",
+    });
+});
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
